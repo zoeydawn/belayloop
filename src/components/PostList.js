ac64de807a25664ab07f34daac04e07eab6c0a6a
Donovan <donovan6925@gmail.com>
2017-03-15 19:47:03 -0400

    filtered out expired posts on PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,17 +4,26 @@ import PostCard from './PostCard';
 const PostsList = (props) => {
   const { posts } = props;
   let list = '';
+  const currentTime = new Date();
+  // console.log('currentTime:', currentTime);
   if (posts) {
     const sortedPosts = Object.keys(posts).sort((a, b) => {
+      // console.log(':', new Date(JSON.parse(posts[a].timestamp)));
       return new Date(JSON.parse(posts[a].timestamp)) - new Date(JSON.parse(posts[b].timestamp));
     });
-    list = sortedPosts.map(post => (
-      <div key={post}>
-        <br />
-        <PostCard post={posts[post]} submitMessage={props.submitMessage} />
-        <br />
-      </div>
-    ));
+    list = sortedPosts.map((post) => {
+      // console.log('posts[post].timestamp:', posts[post].timestamp);
+      if (new Date(JSON.parse(posts[post].timestamp)) - currentTime > 0) {
+        return (
+          <div key={post}>
+            <br />
+            <PostCard post={posts[post]} submitMessage={props.submitMessage} />
+            <br />
+          </div>
+        );
+      }
+      return '';
+    });
   }
   return (
     <div>
